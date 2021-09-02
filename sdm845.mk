9825f03d4838d4127f3e44aeda72229cab92aba6
chokutomoe <rinnetenseilyfe@gmail.com>
2021-09-02 15:21:40 +0000

    sdm845-common: Adapt LOS Overlays

--- sdm845.mk
+++ sdm845.mk
@@ -22,7 +22,6 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
     $(LOCAL_PATH)/overlay-system
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
